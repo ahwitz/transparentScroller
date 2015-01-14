3c44bcfc60d44503499713e9c43f11cf05924b49
ahwitz <ahwitz@gmail.com>
2015-01-14 12:01:40 -0500

    Fixed: dimension calculation for output -Reorganized imports -Added command-line input for input file

--- scroller.py
+++ scroller.py
@@ -1,31 +1,39 @@
 from __future__ import division
-from PIL import Image
 from math import floor
-import os
-from pgmagick import Image as mImage, ImageList
-#import cv2, cv
+import os, sys
+import argparse
 
-#mimage = MagickImage('test.pdf')
-#mimageout = mimage.write('test.png')
+from PIL import Image
+from pgmagick import Image as mImage, ImageList
 
 #alpha 0 is true transparent\
+
+parser = argparse.ArgumentParser(description='Creates a transparent video scrolling through a pdf.')
+parser.add_argument('input', metavar='i', help='input file')
+args = parser.parse_args()
 
 if not os.path.exists('imgout'):
     os.makedirs('imgout')
 
 mimage = mImage()
-mimage.density("200")
+mimage.density("100")
 mimage.quality(100)
-mimage.read('test.pdf')
+try:
+	mimage.read(args.input)
+except RuntimeError:
+	print "ImageMagick threw an error. Make sure the filename is correct."
+	sys.exit()
+print "ImageMagick is converting to png..."
 mimage.write('imgout/temp.png')
 
+print "Loading into PIL..."
 image = Image.open('imgout/temp.png')
 
 sequence = []
 pix_arr = image.load()
-img_x = 1274
-img_y = image.size[1]
-new_img_height = int(floor((img_y / 16) * 9))
+img_x, img_y = image.size
+#convert to 16*9
+new_img_height = int((img_x / 16) * 9)
 
 for cur_start_pos in range(0, img_y - new_img_height):
  	new_img = Image.new("RGBA", (img_x, new_img_height))
@@ -38,6 +46,6 @@ for cur_start_pos in range(0, img_y - new_img_height):
  	while len(start_pos_string) < 5:
  		start_pos_string = "0" + start_pos_string
 
- 	new_img.save("imgout/image" + start_pos_string + ".png")
+ 	new_img.save("imgout/frame" + start_pos_string + ".png")
  	if cur_start_pos % 10 == 0:
  		print "Created", cur_start_pos, "out of", (img_y - new_img_height), "images."
